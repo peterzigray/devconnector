83a844629f0552d9824d600e4580937a092a0634
splitSlice <zigray.peter@gmail.com>
2020-03-16 23:32:10 +0100

    + Routes Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route  GET api/Profile
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+// before res.send('Profile route')
+
+try {
+
+const profile = await Profile.findOne({ user: req.user.id }).populate('user',['name','avatar']);
+
+  if(!profile){
+    return res.status(400).json({ msg: 'There is no profile for this user'});
+  }
+  res.json(profile);
+
+} catch(err){
+  console.error(err.message);
+  res.status(500).send('server error');
+}
+
+
+
+});
+
 
 module.exports = router;
