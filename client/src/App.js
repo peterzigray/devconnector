e8456fab4d7cc7c4fa3673bbf542b52e0c894448
splitSlice <zigray.peter@gmail.com>
2020-04-02 22:22:51 +0200

    redux auth + register

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/layout/Navbar';
+import Landing from './Components/layout/Landing';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import Alert from './Components/layout/Alert';
+import './App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert></Alert>
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
